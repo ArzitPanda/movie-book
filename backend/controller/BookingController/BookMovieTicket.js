bd886d0125906d2aa28f4e6101bd3da8c03b5d68
Arijit panda <73696583+ArzitPanda@users.noreply.github.com>
2023-07-01 01:47:28 +0530

    creating the template for BookMovieTicket

---
+++ backend/controller/BookingController/BookMovieTicket.js
@@ -0,0 +1,17 @@
+
+const user  = require("../../models/user.js")
+const {bookShow} = require("../../models/BookMovies.js")
+
+
+
+
+const BookTicket =(req,res)=>{
+
+
+    const {uid,movieid,hallId,date,slot} = req.body
+
+
+
+
+
+}
